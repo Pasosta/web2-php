f67892713e09b395e861692237ef838a179a66df
Trey Briggs <trey.alan.briggs@gmail.com>
2018-02-10 20:54:43 -0700

    fix php

--- web/budgetProject/budgetHomePage.php
+++ web/budgetProject/budgetHomePage.php
@@ -67,7 +67,7 @@
                     $userName = "UnKnown";
                 }
                 
-                echo "<h1>$userName&apos;s Budgets</h1>";
+                echo "<h1>$user&apos;s Budgets</h1>";
             ?>
         </div>
         
