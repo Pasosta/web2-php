ce2d8978bd55226c07738f4001e8c1ef499f8585
Trey Briggs <trey.alan.briggs@gmail.com>
2018-02-17 20:48:08 -0700

    category creation

--- web/budgetProject/categoryProcessing.php
+++ web/budgetProject/categoryProcessing.php
@@ -17,19 +17,30 @@
     {
         //will need to check to see if these are filled, will do later
         $budid = $_REQUEST['budgetid'];
-        var_dump($budid);
+ 
         $wk1 = htmlspecialchars($_REQUEST['wk1']);
         $wk2 = htmlspecialchars($_REQUEST['wk2']);
         $wk3 = htmlspecialchars($_REQUEST['wk3']);
         $wk4 = htmlspecialchars($_REQUEST['wk4']);
         $name = htmlspecialchars($_REQUEST['name']);
-        var_dump($name);
+
         $stmt = $db->prepare('INSERT INTO categories(categoryname, budgetid) VALUES (:name, :budid)');
         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->bindValue(':budid', $budid, PDO::PARAM_INT);
         $stmt->execute();
         
-        //header('Location: ./budgetHomePage.php');
-        //die();
+        $catid = $db->lastInsertId("categories_id_seq");
+        
+        $weeksstmt = $db->prepare('INSERT INTO public.goals(goalFunds, goalWeek, categoryID, budgetId) VALUES(:wk1, 1, 1, 1),(:wk2, 2, 1, 1),(:wk3, 3, 1, 1),(:wk4, 4, :catid, :budid)');
+        $weeksstmt->bindValue(':wk1', $wk1, PDO::PARAM_STR);
+        $weeksstmt->bindValue(':wk2', $wk2, PDO::PARAM_STR);
+        $weeksstmt->bindValue(':wk3', $wk3, PDO::PARAM_STR);
+        $weeksstmt->bindValue(':wk4', $wk4, PDO::PARAM_STR);
+        $weeksstmt->bindValue(':catid', $wk3, PDO::PARAM_STR);
+        $weeksstmt->bindValue(':budid', $budid, PDO::PARAM_INT);
+        
+        $weeksstmt->execute();
+        header('Location: ./budgetHomePage.php');
+        die();
     }
 ?>
