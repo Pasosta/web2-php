66f69c5719da2fcf8e8bc9de0f26c9f8c11674d6
Trey Briggs <trey.alan.briggs@gmail.com>
2018-02-17 20:14:44 -0700

    category creation

--- web/budgetProject/categoryProcessing.php
+++ web/budgetProject/categoryProcessing.php
@@ -16,7 +16,7 @@
     if(isset($_REQUEST['create']))
     {
         //will need to check to see if these are filled, will do later
-        $budid = $_GET['budgetid']
+        $budid = $_GET['budgetid'];
         $wk1 = htmlspecialchars($_REQUEST['wk1']);
         $wk2 = htmlspecialchars($_REQUEST['wk2']);
         $wk3 = htmlspecialchars($_REQUEST['wk3']);
@@ -27,8 +27,6 @@
         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->bindValue(':budid', $budid, PDO::PARAM_INT);
         $stmt->execute();
-        
-        
         
         header('Location: ./budgetHomePage.php');
         die();
