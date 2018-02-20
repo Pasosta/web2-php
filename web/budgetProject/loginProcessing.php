ac1dceda708aa499b705a905d21b742184254b98
Trey Briggs <trey.alan.briggs@gmail.com>
2018-02-20 10:52:31 -0700

    authentication

--- web/budgetProject/loginProcessing.php
+++ web/budgetProject/loginProcessing.php
@@ -7,22 +7,22 @@
         
         
         $useridStmt = $db->prepare('SELECT password FROM users WHERE username = :username');
-        $useridStmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $useridStmt->bindValue(':username', $user, PDO::PARAM_STR);
         $useridStmt->execute();
         
         $dbpass = $useridStmt->fetch(PDO::FETCH_ASSOC);
         
-//        if (password_verify($inputpass, $dbpass)) {
-//            if(!isset($_SESSION['user']))
-//            {
-//                $_SESSION['user'] = $user;
-//            }
-//            header('Location: ./budgetHomePage.php');
-//            die();
-//        } else {
-//            header('Location: ./login.php');
-//            die();
-//        }
+        if (password_verify($inputpass, $dbpass)) {
+            if(!isset($_SESSION['user']))
+            {
+                $_SESSION['user'] = $user;
+            }
+            header('Location: ./budgetHomePage.php');
+            die();
+        } else {
+            header('Location: ./login.php');
+            die();
+        }
         
         
     }
