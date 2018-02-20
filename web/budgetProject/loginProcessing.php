6bd0af6dad23f86697109790f809d7a523d5a3f1
Trey Briggs <trey.alan.briggs@gmail.com>
2018-02-20 10:54:34 -0700

    authentication

--- web/budgetProject/loginProcessing.php
+++ web/budgetProject/loginProcessing.php
@@ -6,24 +6,23 @@
         $inputpass = htmlspecialchars($_REQUEST['passwordBox']);
         
         
-        $useridStmt = $db->prepare('SELECT password FROM users WHERE username = :username');
-        $useridStmt->bindValue(':username', $user, PDO::PARAM_STR);
-        $useridStmt->execute();
-        
-        $dbpass = $useridStmt->fetch(PDO::FETCH_ASSOC);
-        
-        if (password_verify($inputpass, $dbpass)) {
-            if(!isset($_SESSION['user']))
-            {
-                $_SESSION['user'] = $user;
-            }
-            header('Location: ./budgetHomePage.php');
-            die();
-        } else {
-            header('Location: ./login.php');
-            die();
-        }
-        
+//        $useridStmt = $db->prepare('SELECT password FROM users WHERE username = :username');
+//        $useridStmt->bindValue(':username', $user, PDO::PARAM_STR);
+//        $useridStmt->execute();
+//        
+//        $dbpass = $useridStmt->fetch(PDO::FETCH_ASSOC);
+//        
+//        if (password_verify($inputpass, $dbpass)) {
+//            if(!isset($_SESSION['user']))
+//            {
+//                $_SESSION['user'] = $user;
+//            }
+//            header('Location: ./budgetHomePage.php');
+//            die();
+//        } else {
+//            header('Location: ./login.php');
+//            die();
+//        }
         
     }
 ?>
